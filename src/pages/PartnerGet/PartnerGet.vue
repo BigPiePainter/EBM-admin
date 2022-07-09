e5bf33c29b8dc4973c6a4c6a8d4fd7dc56afb906
Zack <zqy21@qq.com>
2022-07-09 15:41:43 +0800

    add secondary table expand

--- src/pages/PartnerGet/PartnerGet.vue
+++ src/pages/PartnerGet/PartnerGet.vue
@@ -16,17 +16,32 @@
     </v-card-title>
 
     <v-data-table
+      single-expand
+      show-expand
+      loading
+      loading-text="加载中... 请稍后"
       :headers="headers"
       :items="products"
-      single-expand
       :expanded.sync="expanded"
       item-key="name"
-      show-expand
       sort-by="name"
       class="elevation-1"
+      @click:row="clickRow"
     >
       <template v-slot:expanded-item="{ headers, item }">
-        <td :colspan="headers.length">More info about {{ item.name }}</td>
+        <td :colspan="headers.length">
+          <div>
+            <v-data-table
+              hide-default-footer
+              :headers="sheaders"
+              :items="desserts"
+              :a="headers"
+              :b="item"
+              dense
+              class="elevation-1"
+            ></v-data-table>
+          </div>
+        </td>
       </template>
 
       <template v-slot:top>
@@ -330,6 +345,102 @@ export default {
       u: "",
       i: "",
     },
+
+    sheaders: [
+      {
+        text: "SKU名称",
+        align: "start",
+        sortable: false,
+        value: "name",
+      },
+      { text: "Calories", value: "calories" },
+      { text: "Fat (g)", value: "fat" },
+      { text: "Carbs (g)", value: "carbs" },
+      { text: "Protein (g)", value: "protein" },
+      { text: "Iron (%)", value: "iron" },
+    ],
+    desserts: [
+      {
+        name: "尺寸:20寸【直接登机】送运费险*不满意包退;颜色分类:【商务黑】直角拉链款",
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        iron: "1%",
+      },
+      {
+        name: "Ice cream sandwich",
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        iron: "1%",
+      },
+      {
+        name: "Eclair",
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        iron: "7%",
+      },
+      {
+        name: "Cupcake",
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        iron: "8%",
+      },
+      {
+        name: "Gingerbread",
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        iron: "16%",
+      },
+      {
+        name: "Jelly bean",
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        iron: "0%",
+      },
+      {
+        name: "Lollipop",
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        iron: "2%",
+      },
+      {
+        name: "Honeycomb",
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        iron: "45%",
+      },
+      {
+        name: "Donut",
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        iron: "22%",
+      },
+      {
+        name: "KitKat",
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        iron: "6%",
+      },
+    ],
   }),
 
   computed: {
@@ -352,7 +463,16 @@ export default {
   },
 
   methods: {
+    clickRow(item, event) {
+      if (event.isExpanded) {
+        const index = this.expanded.findIndex((i) => i === item);
+        this.expanded.splice(index, 1);
+      } else {
+        this.expanded.push(item);
+      }
+    },
     initialize() {
+      this.products = [];
       this.products = [
         {
           name: "334455461868",
