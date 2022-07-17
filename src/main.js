84d487bccead4d018e3476aca25381d7810a2a25
Zack <zqy21@qq.com>
2022-07-17 07:38:13 -0300

    删除无用包

--- src/main.js
+++ src/main.js
@@ -3,18 +3,11 @@ import App from './App.vue'
 import router from './Routes'
 import store from './store/index'
 import vuetify from './plugins/vuetify'
-import * as VueGoogleMaps from 'vue2-google-maps';
 import Toast from "vue-toastification";
 import Global from './global'
 import "vue-toastification/dist/index.css";
 
 Vue.use(Toast);
-
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyB7OXmzfQYua_1LEhRdqsoYzyJOPh9hGLg',
-  },
-});
 
 Vue.config.productionTip = false
 
