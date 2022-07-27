d4cef4802198e74287b6e92c38f7683f71d1f00e
Zack <zqy21@qq.com>
2022-07-27 03:45:41 -0300

    厂家模块开发

--- src/components/SkuTable/SkuTable.vue
+++ src/components/SkuTable/SkuTable.vue
@@ -416,16 +416,6 @@ export default {
       loading: false,
 
       check: true,
-
-      rules: {
-        required: (value) => !!value || "必填",
-        counter: (value) => value.length <= 20 || "Max 20 characters",
-        email: (value) => {
-          const pattern =
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          return pattern.test(value) || "Invalid e-mail.";
-        },
-      },
     };
   },
 
