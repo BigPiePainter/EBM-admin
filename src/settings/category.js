fabb696cab95052a1b73dde7dae994931d60468d
Niggoss <74089601+Niggoss@users.noreply.github.com>
2022-08-08 19:24:33 +0800

    111111

--- src/settings/category.js
+++ src/settings/category.js
@@ -8,3 +8,30 @@ export const getCategory = data => {
     }
     return http.request(args)
 }
+
+export const addCategory = data => {
+    const args = {
+        url: '/category/add',
+        method: 'POST',
+        data
+    }
+    return http.request(args)
+}
+
+export const editCategory = data => {
+    const args = {
+        url: '/category/edit',
+        method: 'POST',
+        data
+    }
+    return http.request(args)
+}
+
+export const deleteCategory = data => {
+    const args = {
+        url: '/category/delete',
+        method: 'POST',
+        data
+    }
+    return http.request(args)
+}
