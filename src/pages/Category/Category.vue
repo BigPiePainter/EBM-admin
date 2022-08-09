3364ac85b7f5a6eb6837ab3d547208272ed9936f
Niggoss <74089601+Niggoss@users.noreply.github.com>
2022-08-10 04:30:24 +0800

    ngdrshjt

--- src/pages/Category/Category.vue
+++ src/pages/Category/Category.vue
@@ -4,11 +4,11 @@
       <v-data-table
         fixed-header
         no-data-text="空"
-        item-key="category"
+        item-key="uid"
         show-expand
         height="calc(100vh - 200px)"
         :headers="categoryHeaders"
-        :items="categoryItems"
+        :items="calculatedCategorys"
         disable-sort
       >
         <template v-slot:top>
@@ -50,7 +50,7 @@
                     </v-text-field>
                   </v-col>
 
-                  <v-col cols="12">
+                  <v-col cols="12" :hidden="newCate">
                     <span class="text-body-2 text--secondary">品类扣点*</span>
                     <v-text-field
                       outlined
@@ -60,7 +60,7 @@
                     ></v-text-field>
                   </v-col>
 
-                  <v-col cols="12">
+                  <v-col cols="12" :hidden="newCate">
                     <span class="text-body-2 text--secondary">品类运费险*</span>
                     <v-text-field
                       outlined
@@ -70,7 +70,7 @@
                     ></v-text-field>
                   </v-col>
 
-                  <v-col cols="12">
+                  <v-col cols="12" :hidden="newCate">
                     <span class="text-body-2 text--secondary">
                       选择生效日期*
                     </span>
@@ -114,6 +114,17 @@
                         </v-btn>
                       </v-date-picker>
                     </v-menu>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <span class="text-body-2 text--secondary">备注</span>
+                    <v-text-field
+                      outlined
+                      dense
+                      hide-details
+                      v-model="editedItem.note"
+                    >
+                    </v-text-field>
                   </v-col>
                 </v-col>
 
@@ -176,7 +187,17 @@
           </v-dialog>
         </template>
         <template v-slot:expanded-item="{ headers, item }">
-          <td :colspan="headers.length">More info about {{ item.name }}</td>
+          <td :colspan="headers.length" class="px-0">
+            <v-card tile elevation="0">
+              <v-data-table
+                :headers="subHeaders"
+                :items="
+                  allCategoryHistorys.filter((i) => item.uid == i.categoryId)
+                "
+              >
+              </v-data-table>
+            </v-card>
+          </td>
         </template>
       </v-data-table>
     </v-card>
@@ -186,8 +207,10 @@
 <script>
 import { getCategory } from "@/settings/category";
 import { addCategory } from "@/settings/category";
-import { editCategory } from "@/settings/category";
-import { deleteCategory } from "@/settings/category";
+import { addHistoryCategory } from "@/settings/category";
+import { getHistoryCategory } from "@/settings/category";
+import { javaUTCDateToString } from "@/libs/utils";
+
 export default {
   data() {
     return {
@@ -201,25 +224,44 @@ export default {
         { text: "一级类目", value: "name" },
         { text: "品类扣点", value: "deduction" },
         { text: "品类运费险", value: "insurance" },
-        { text: "生效时间", value: "startTime" },
+        { text: "备注", value: "note" },
         { text: "操作", value: "actions" },
       ],
-      categoryItems: [],
+      calculatedCategorys: [],
+
+      allCategorys: [],
+      subHeaders: [
+        { text: "品类扣点", value: "deduction" },
+        { text: "品类运费险", value: "insurance" },
+        { text: "生效时间", value: "calculatedStartTime" },
+        { text: "备注", value: "note" },
+      ],
+      subItems: [],
+      allCategoryHistorys: [],
+      newCate: true,
+      done: [false, false],
     };
   },
 
   created() {
     this.loadData();
-    //console.log(this.categoryItems);
+    console.log(this.editedItem.uid);
   },
 
   computed: {
     isEmp: function () {
+      var check1 = ["name"];
       var check = ["name", "deduction", "insurance", "startTime"];
       var pass = true;
-      check.forEach((item) => {
-        if (!this.editedItem[item]) pass = false;
-      });
+      if (this.mode == 1) {
+        check1.forEach((item) => {
+          if (!this.editedItem[item]) pass = false;
+        });
+      } else if (this.mode == 2) {
+        check.forEach((item) => {
+          if (!this.editedItem[item]) pass = false;
+        });
+      }
       //console.log(pass);
       return !pass;
     },
@@ -229,53 +271,123 @@ export default {
     dayFormat(date) {
       return Number(date.split("-")[2]);
     },
-
     loadData() {
+      this.done.fill(false);
+      getHistoryCategory({}).then((res) => {
+        this.allCategoryHistorys = res.data.categoryHistorys;
+        console.log(this.allCategoryHistorys);
+        this.requestDone(0);
+      });
       getCategory({}).then((res) => {
-        this.categoryItems = res.data.categorys;
+        this.allCategorys = res.data.categorys;
+        console.log(this.allCategorys);
+        console.log(this.allCategorys[0]);
+        this.requestDone(1);
       });
     },
-
+    requestDone(i) {
+      this.done[i] = true;
+      console.log("done");
+      if (this.done.find((i) => !i) == false) return;
+      this.dataAnalyze();
+    },
+    //----------------------------------------------------------------------------------------
+    dataAnalyze() {
+      this.allCategoryHistorys.forEach((i) => {
+        i.calculatedStartTime = javaUTCDateToString(i.startTime);
+      });
+      console.log("dataAnalyze1");
+      this.calculatedCategorys = [];
+      this.allCategorys.forEach((i) => {
+        this.calculatedCategorys.push({
+          name: i.name,
+          deduction: this.newest(this.allCategoryHistorys, i.uid).deduction,
+          insurance: this.newest(this.allCategoryHistorys, i.uid).insurance,
+          note: i.note,
+          uid: i.uid,
+        });
+      });
+      // console.log(this.calculatedCategorys);
+      // console.log(this.allCategorys);
+      // //this.calculatedCategorys = this.allCategorys;
+      // console.log("dataAnalyze");
+      // console.log(this.calculatedCategorys);
+      // console.log(this.allCategorys);
+      // for (let i = 0; i < this.allCategorys.length; i++) {
+      //   this.calculatedCategorys[i] = {};
+      //    this.subItems[i] = [];
+      //    for (let j = 0; j < this.allCategoryHistorys.length; j++) {
+      //      if (this.allCategoryHistorys[j].categoryId == this.allCategorys[i].uid) {
+      //        this.subItems[i].push(this.allCategoryHistorys[j]);
+      //      }
+      //    }
+      // }
+      //        if (
+      //          this.allCategoryHistorys[j + 1].startTime >
+      //          this.allCategoryHistorys[k].startTime
+      //        ) {
+      //          k = j + 1;
+      //        }
+      //     }
+      //    }
+      //    this.calculatedCategorys[i].note = this.allCategorys[i].note;
+      //    this.calculatedCategorys[i].name = this.allCategorys[i].name;
+      //     this.calculatedCategorys[i].calculatedStartTime = this.allCategoryHistorys[k].calculatedStartTime;
+      //     this.calculatedCategorys[i].deduction = this.allCategoryHistorys[k].deduction;
+      //     this.calculatedCategorys[i].insurance = this.allCategoryHistorys[k].insurance;
+      // }
+      // console.log(this.calculatedCategorys);
+      // console.log(this.subItems);
+    },
     //----------------------------------------------------------------------------------------
     addButton() {
       this.mode = 1;
       this.editedItem = [];
       this.checkReadOnly = false;
+      this.newCate = true;
     },
     editButton(item) {
       this.mode = 2;
       this.editedItem = Object.assign({}, item);
+      console.log(this.editedItem);
       this.checkReadOnly = true;
       this.dialog = true;
+      this.newCate = false;
     },
     close() {
       this.dialog = false;
     },
     save() {
+      console.log(this.mode);
       if (this.mode == 1) {
         this.add();
       } else if (this.mode == 2) {
         this.edit();
+        console.log(this.editedItem);
       }
     },
     add() {
       var args = { ...this.editedItem };
+      args.categoryId = args.uid;
       args.startTime = args.startTime.replaceAll("-", "/");
-      console.log(args);
       addCategory(args)
         .then((res) => {
           this.global.infoAlert("泼发EBC：" + res.data);
-          this.loadData();
         })
         .catch(() => {
           setTimeout(() => {
             this.global.infoAlert("泼发EBC：上传失败");
+            this.loadData();
           }, 100);
         });
       this.dialog = false;
     },
     edit() {
-      editCategory(this.editedItem)
+      var args = { ...this.editedItem };
+      args.categoryId = args.uid;
+      args.startTime = args.startTime.replaceAll("-", "/");
+      console.log(args);
+      addHistoryCategory(args)
         .then((res) => {
           this.global.infoAlert("泼发EBC：" + res.data);
           this.loadData();
@@ -298,8 +410,15 @@ export default {
       this.deleteDialog = false;
       this.delete();
     },
-    delete() {
-      deleteCategory({});
+    delete() {},
+
+    newest(array, id) {
+      var newest = { startTime: 0, deduction: "无数据", insurance: "无数据" };
+      array.forEach((i) => {
+        if (i.uid != id) return;
+        i.startTime >= newest.startTime && (newest = i);
+      });
+      return newest;
     },
   },
 };
